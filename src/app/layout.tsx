a5d3d5f08dfeeb442b9e693cdd461c092ad8c249
githong <githong@kakao.com>
2024-08-25 09:26:10 +0900

    remove responsive css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,12 @@ export default function RootLayout({
       <body>
         <div className="flex min-h-screen w-full flex-col">
           <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-            <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+            <nav>
               <Link
                 href="/"
                 className="flex items-center justify-center space-x-2 transition-colors hover:text-orange-400"
               >
-                <div className="flex items-center gap-2 text-lg font-semibold md:text-base">
+                <div className="flex items-center gap-2 text-lg font-semibold">
                   <House className="h-6 w-6" />
                   <span className="sr-only">Acme Inc</span>
                 </div>
